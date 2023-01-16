0273b5277ebde42cbbcc4cda756a98f152cc7127
badda-bing <92822024+badda-bing@users.noreply.github.com>
2023-01-16 13:58:44 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,5 +37,5 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What would you like to add?','jackfruit')
-my_cur.execute("insert into fruit_load_list values(" + add_my_fruit + ")"
+my_cur.execute("insert into fruit_load_list values(" + add_my_fruit + ")")
 streamlit.write('entered ', add_my_fruit)
